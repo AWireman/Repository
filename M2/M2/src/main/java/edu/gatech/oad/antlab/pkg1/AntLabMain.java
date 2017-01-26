4a291de636f7c5591aec22b462b4492d9a617b9b
jcelliott17 <jcelliott17@gatech.edu>
2017-01-25 20:46:08 -0500

    Updated AntLabMain

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,14 +67,16 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("P3");
 		  toPrint += p3.toString("gburdell3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+          //Person4 replace P4 with your name
+                //and gburdell4 with your gt id
+                  Person4 p4 = new Person4("Jacob Butler");
+                  toPrint += p4.toString("jbutler72");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
+                  Person5 p5 = new Person6("P5");
                   toPrint += p5.toString("gburdell5");
+                  Person6 p6 = new Person6("Jacqueline Elliott");
+                  toPrint += p6.toString("jelliott40");
 		  
         System.out.println(toPrint);
         
@@ -89,9 +91,5 @@ import edu.gatech.oad.antlab.pkg2.*;
         
         new AntLabMain().printOutMessage();
         
-     } 
-    
-    
-    
-    
+     }
  } 
